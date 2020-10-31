a95a8160a43d0bf61e94a478cf654362f12b93a7
Duane Byer <duane.byer@gmail.com>
2020-10-31 18:55:04 -0400

    Fix all the bugs

--- main.cpp
+++ main.cpp
@@ -55,25 +55,34 @@ Index3 mod(Index3 xs, unsigned m) {
 	return xs;
 }
 
+enum class Occupied {
+	EMPTY,
+	LINE,
+	HEAD,
+	TAIL,
+	MERGE,
+};
+
 struct Lattice {
-	std::vector<bool> _occupied;
+	std::vector<Occupied> _occupied;
 	std::vector<Index3> _prev_site;
 	std::vector<Index3> _next_site;
 	unsigned N;
 	unsigned k;
 
 	Lattice(unsigned N, unsigned k) :
-		_occupied(k * N * N * N, false),
+		_occupied(k * N * N * N, Occupied::EMPTY),
 		_prev_site(k * N * N * N, Index3 { }),
 		_next_site(k * N * N * N, Index3 { }),
-	       N(N), k(k) { }
+		N(N), k(k) {
+	}
 
-	std::vector<bool>::const_reference occupied(int t, Index3 index) const {
+	Occupied const& occupied(int t, Index3 index) const {
 		t = mod(t, k);
 		index = mod(index, N);
 		return _occupied[N * N * N * t + N * N * index.x + N * index.y + index.z];
 	}
-	std::vector<bool>::reference occupied(int t, Index3 index) {
+	Occupied& occupied(int t, Index3 index) {
 		t = mod(t, k);
 		index = mod(index, N);
 		return _occupied[N * N * N * t + N * N * index.x + N * index.y + index.z];
@@ -99,18 +108,58 @@ struct Lattice {
 		return _next_site[N * N * N * t + N * N * index.x + N * index.y + index.z];
 	}
 
-	void print_lattice() const {
+	void print_lattice(bool show_occupations=false) const {
 		for (unsigned t = 0; t < k; ++t) {
 			for (unsigned y = 0; y < N; ++y) {
 				for (unsigned z = 0; z < N; ++z) {
 					for (unsigned x = 0; x < N; ++x) {
-						if (occupied(t, { x, y, z })) {
-							std::cout << "#";
-						} else {
-							std::cout << ".";
+						Index3 next = next_site(t, { x, y, z });
+						if (show_occupations) {
+							switch (occupied(t, { x, y, z })) {
+							case Occupied::EMPTY:
+								std::cout << 'E';
+								break;
+							case Occupied::LINE:
+								std::cout << 'L';
+								break;
+							case Occupied::TAIL:
+								std::cout << 'T';
+								break;
+							case Occupied::HEAD:
+								std::cout << 'H';
+								break;
+							case Occupied::MERGE:
+								std::cout << 'M';
+								break;
+							}
+						}
+						switch (occupied(t, { x, y, z })) {
+						case Occupied::EMPTY:
+							std::cout << '.';
+							break;
+						case Occupied::LINE:
+						case Occupied::TAIL:
+						case Occupied::MERGE:
+							if (mod(next.x, N) == x + 1) {
+								std::cout << "→";
+							} else if(mod(next.x, N) == x - 1) {
+								std::cout << "←";
+							} else if (mod(next.y, N) == y + 1) {
+								std::cout << "↓";
+							} else if (mod(next.y, N) == y - 1) {
+								std::cout << "↑";
+							} else if (mod(next.z, N) == z + 1) {
+								std::cout << "⊙";
+							} else if (mod(next.z, N) == z - 1) {
+								std::cout << "⊗";
+							}
+							break;
+						case Occupied::HEAD:
+							std::cout << 'H';
+							break;
 						}
 					}
-					std::cout << " ";
+					std::cout << ' ';
 				}
 				std::cout << std::endl;
 			}
@@ -122,18 +171,20 @@ struct Lattice {
 int main(int argc, char** argv) {
 	// Initialize lattice.
 	unsigned N = 2;    // Number of lattice sites along one axis.
-	unsigned K = 2; // Number of time steps.
+	unsigned K = 1000; // Number of time steps.
 	unsigned D = 3;    // Dimension of lattice.
 	double T = 1.;     // Hopping constant.
 	double mu = 1.4;    // Chemical potential.
 	double beta = 12.;  // Beta.
 	double epsilon = beta / K;
-	unsigned events_burn = 0;
-	unsigned event_count = 3200;
+	unsigned events_burn = 3000;
+	unsigned event_count = 15200;
 	Lattice lattice(N, K);
 	// Random number engine.
 	std::default_random_engine rng;
-	rng.seed(0);
+	int seed = std::time(0);
+	rng.seed(seed);
+	std::cout << "Using random seed " << seed << "." << std::endl;
 	std::uniform_real_distribution<double> prob_dist(0., 1.);
 	std::uniform_int_distribution<int> N_dist(0, N - 1);
 	std::uniform_int_distribution<int> K_dist(0, K - 1);
@@ -144,7 +195,7 @@ int main(int argc, char** argv) {
 	std::vector<unsigned> winding_numbers;
 	// State variables.
 	bool forwards = true;
-	Index3 next_xs = { 0, 0, 0 };
+	Index3 merge_prev_xs = { 0, 0, 0 };
 	Index3 winding = { 0, 0, 0 };
 	unsigned hop_space = 0;
 	for (unsigned idx = 0; idx < event_count; ++idx) {
@@ -158,14 +209,19 @@ int main(int argc, char** argv) {
 		Index3 xs = start_xs;
 		int t = start_t;
 		// Check if occupied to set our initial direction correctly.
-		if (lattice.occupied(t, xs)) {
-			forwards = false;
-			next_xs = lattice.prev_site(t, xs);
-		} else {
+		switch (lattice.occupied(t, xs)) {
+		case Occupied::EMPTY:
 			forwards = true;
-			lattice.occupied(t, xs) = true;
+			break;
+		case Occupied::LINE:
+			forwards = false;
+			break;
+		default:
+			std::cerr << "Error 9" << std::endl;
+			return 9;
 		}
-		while (true) {
+		bool terminate = false;
+		while (!terminate) {
 			if (LOG) {
 				std::cout << "hops: " << hop_space << std::endl;
 				std::cout << "pos: " << t << ", (" << xs[0] << ", " << xs[1] << ", " << xs[2] << ")" << std::endl;
@@ -174,88 +230,141 @@ int main(int argc, char** argv) {
 			// Evolve.
 			if (forwards) {
 				if (prob_dist(rng) <= std::exp(mu * epsilon)) {
+					if (LOG) {
+						std::cout << "forwards accept" << std::endl;
+					}
 					// Accept.
-					// Choose direction.
-					next_xs = xs;
-					if (prob_dist(rng) > 1. - 2. * T * D * epsilon) {
-						// Hop to different site.
-						hop_space += 1;
-						unsigned dir = dir_dist(rng);
-						int sign = prob_dist(rng) < 0.5 ? 1 : -1;
-						next_xs[dir] += sign;
-						if (next_xs[dir] < 0) {
-							next_xs[dir] += N;
-							winding[dir] += 1;
-						}
-						if (next_xs[dir] >= N) {
-							next_xs[dir] -= N;
-							winding[dir] -= 1;
-						}
-					}
-					bool old_occupied = lattice.occupied(t + 1, next_xs);
-					Index3 old_prev_site = lattice.prev_site(t + 1, next_xs);
-					// Create a path to next site.
-					lattice.occupied(t + 1, next_xs) = true;
-					lattice.prev_site(t + 1, next_xs) = xs;
-					lattice.next_site(t, xs) = next_xs;
-					xs = next_xs;
-					t += 1;
-					if (t >= K) {
-						t -= K;
-					}
-					if (old_occupied) {
-						// Check finished.
-						if (start_t == mod(t, K) && start_xs == mod(xs, N)) {
-							if (LOG) {
-								std::cout << "Terminate forward." << std::endl;
+					Occupied occupied = lattice.occupied(t, xs);
+					if (occupied == Occupied::EMPTY || occupied == Occupied::HEAD) {
+						// Choose direction.
+						Index3 next_xs = xs;
+						if (prob_dist(rng) > 1. - 2. * T * D * epsilon) {
+							// Hop to different site.
+							hop_space += 1;
+							unsigned dir = dir_dist(rng);
+							int sign = prob_dist(rng) < 0.5 ? 1 : -1;
+							next_xs[dir] += sign;
+							if (next_xs[dir] < 0) {
+								next_xs[dir] += N;
+								winding[dir] += 1;
 							}
-							break;
+							if (next_xs[dir] >= N) {
+								next_xs[dir] -= N;
+								winding[dir] -= 1;
+							}
 						}
-						// Flip direction and start moving backwards.
-						forwards = false;
-						next_xs = old_prev_site;
+						// Create a path to next site.
+						Occupied next_occupied = lattice.occupied(t + 1, next_xs);
+						if (next_occupied == Occupied::EMPTY) {
+							// Propogate.
+							lattice.occupied(t + 1, next_xs) = Occupied::HEAD;
+						} else if (next_occupied == Occupied::LINE) {
+							// Merge.
+							forwards = false;
+							lattice.occupied(t + 1, next_xs) = Occupied::MERGE;
+							merge_prev_xs = lattice.prev_site(t + 1, next_xs);
+						} else if (next_occupied == Occupied::TAIL) {
+							// Terminate.
+							lattice.occupied(t + 1, next_xs) = Occupied::LINE;
+							if (LOG) {
+								std::cout << "terminate forward hit tail" << std::endl;
+							}
+							terminate = true;
+						} else {
+							std::cerr << "Error 1" << std::endl;
+							return 1;
+						}
+						if (occupied == Occupied::EMPTY) {
+							lattice.occupied(t, xs) = Occupied::TAIL;
+						} else {
+							lattice.occupied(t, xs) = Occupied::LINE;
+						}
+						lattice.next_site(t, xs) = next_xs;
+						lattice.prev_site(t + 1, next_xs) = xs;
+						xs = next_xs;
+						t += 1;
+						if (t >= K) {
+							t -= K;
+						}
+					} else {
+						std::cerr << "Error 2" << std::endl;
+						return 2;
 					}
 				} else {
-					// Reject.
+					if (LOG) {
+						std::cout << "forwards reject" << std::endl;
+					}
 					forwards = false;
-					next_xs = lattice.prev_site(t, xs);
+					Occupied occupied = lattice.occupied(t, xs);
+					if (occupied == Occupied::HEAD) {
+					} else if (occupied == Occupied::EMPTY) {
+						// Terminate.
+						if (LOG) {
+							std::cout << "terminate forward immediate" << std::endl;
+						}
+						terminate = true;
+					} else {
+						std::cerr << "Error 3" << std::endl;
+						return 3;
+					}
 				}
 			} else {
 				// Moving backwards.
 				if (prob_dist(rng) <= std::exp(-mu * epsilon)) {
+					if (LOG) {
+						std::cout << "backwards accept" << std::endl;
+					}
 					// Accept.
+					Occupied occupied = lattice.occupied(t, xs);
+					Index3 next_xs;
+					if (occupied == Occupied::LINE) {
+						next_xs = lattice.prev_site(t, xs);
+						lattice.occupied(t, xs) = Occupied::TAIL;
+					} else if (occupied == Occupied::HEAD) {
+						next_xs = lattice.prev_site(t, xs);
+						lattice.occupied(t, xs) = Occupied::EMPTY;
+					} else if (occupied == Occupied::MERGE) {
+						next_xs = merge_prev_xs;
+						lattice.occupied(t, xs) = Occupied::LINE;
+					} else {
+						std::cerr << "Error 4" << std::endl;
+						return 4;
+					}
+					Occupied next_occupied = lattice.occupied(t - 1, next_xs);
+					if (next_occupied == Occupied::LINE) {
+						lattice.occupied(t - 1, next_xs) = Occupied::HEAD;
+					} else if (next_occupied == Occupied::TAIL) {
+						lattice.occupied(t - 1, next_xs) = Occupied::EMPTY;
+						if (LOG) {
+							std::cout << "terminate backwards tail" << std::endl;
+						}
+						terminate = true;
+					} else {
+						std::cerr << "Error 5" << std::endl;
+						return 5;
+					}
 					if (xs != next_xs) {
 						hop_space -= 1;
 					}
-					if (lattice.prev_site(t, xs) == next_xs) {
-						lattice.occupied(t, xs) = false;
-					}
 					xs = next_xs;
 					t -= 1;
 					if (t < 0) {
 						t += K;
-					}
-					next_xs = lattice.prev_site(t, xs);
-					// Check finished.
-					if (start_t == mod(t, K) && start_xs == mod(xs, N)) {
-						if (LOG) {
-							std::cout << "Terminate reverse." << std::endl;
-						}
-						lattice.occupied(t, xs) = false;
-						break;
 					}
 				} else {
+					if (LOG) {
+						std::cout << "backwards reject" << std::endl;
+					}
 					// Reject.
 					forwards = true;
-					// Clear current position and step back.
-					lattice.occupied(t, xs) = false;
-					xs = next_xs;
-					t -= 1;
-					if (t < 0) {
-						t += K;
+					Occupied occupied = lattice.occupied(t, xs);
+					// Choose direction.
+					Index3 next_xs;
+					if (occupied == Occupied::LINE || occupied == Occupied::HEAD) {
+						next_xs = lattice.prev_site(t, xs);
+					} else if (occupied == Occupied::MERGE) {
+						next_xs = merge_prev_xs;
 					}
-					// Do a forced step forwards (no rejection).
-					next_xs = xs;
 					if (prob_dist(rng) > 1. - 2. * T * D * epsilon) {
 						// Hop to different site.
 						hop_space += 1;
@@ -271,30 +380,68 @@ int main(int argc, char** argv) {
 							winding[dir] -= 1;
 						}
 					}
-					bool old_occupied = lattice.occupied(t + 1, next_xs);
-					Index3 old_prev_site = lattice.prev_site(t + 1, next_xs);
-					// Create a path to next site.
-					lattice.occupied(t + 1, next_xs) = true;
-					lattice.prev_site(t + 1, next_xs) = xs;
-					lattice.next_site(t, xs) = next_xs;
-					xs = next_xs;
-					t += 1;
-					if (t >= K) {
-						t -= K;
-					}
-					if (old_occupied) {
-						// Check finished.
-						if (start_t == mod(t, K) && start_xs == mod(xs, N)) {
-							if (LOG) {
-								std::cout << "Terminate reverse forward." << std::endl;
+					Occupied next_occupied = lattice.occupied(t, next_xs);
+					if (occupied == Occupied::LINE
+							|| occupied == Occupied::HEAD
+							|| occupied == Occupied::MERGE) {
+						if (next_xs == xs) {
+							if (occupied == Occupied::LINE) {
+								// Terminate.
+								if (LOG) {
+									std::cout << "terminate backwards immediate" << std::endl;
+								}
+								terminate = true;
+							} else if (occupied == Occupied::HEAD) {
+							} else if (occupied == Occupied::MERGE) {
+								forwards = false;
+								Index3 prev_xs = merge_prev_xs;
+								merge_prev_xs = lattice.prev_site(t, next_xs);
+								lattice.next_site(t - 1, prev_xs) = next_xs;
+								lattice.prev_site(t, next_xs) = prev_xs;
 							}
-							break;
+						} else {
+							Index3 prev_xs;
+							if (occupied == Occupied::MERGE) {
+								prev_xs = merge_prev_xs;
+							} else {
+								prev_xs = lattice.prev_site(t, xs);
+							}
+							if (next_occupied == Occupied::EMPTY
+									|| next_occupied == Occupied::HEAD) {
+								lattice.occupied(t, next_xs) = Occupied::HEAD;
+							} else if (next_occupied == Occupied::LINE) {
+								forwards = false;
+								lattice.occupied(t, next_xs) = Occupied::MERGE;
+								merge_prev_xs = lattice.prev_site(t, next_xs);
+							} else if ((occupied == Occupied::HEAD || occupied == Occupied::MERGE)
+									&& next_occupied == Occupied::TAIL) {
+								// Terminate.
+								lattice.occupied(t, next_xs) = Occupied::LINE;
+								if (LOG) {
+									std::cout << "terminate backwards reject" << std::endl;
+								}
+								terminate = true;
+							} else if (next_occupied == Occupied::MERGE) {
+								forwards = false;
+								merge_prev_xs = lattice.prev_site(t, next_xs);
+							} else {
+								std::cerr << "Error 6" << std::endl;
+								return 6;
+							}
+							if (occupied == Occupied::LINE) {
+								lattice.occupied(t, xs) = Occupied::TAIL;
+							} else if (occupied == Occupied::MERGE) {
+								lattice.occupied(t, xs) = Occupied::LINE;
+							} else {
+								lattice.occupied(t, xs) = Occupied::EMPTY;
+							}
+							lattice.next_site(t - 1, prev_xs) = next_xs;
+							lattice.prev_site(t, next_xs) = prev_xs;
+							xs = next_xs;
 						}
-						// Flip direction and start moving backwards.
-						forwards = false;
-						next_xs = old_prev_site;
 					} else {
-						// Good.
+						std::cerr << "Error 7" << std::endl;
+						return 7;
 					}
 				}
 			}
@@ -304,12 +451,48 @@ int main(int argc, char** argv) {
 			lattice.print_lattice();
 		}
 		// Compute observables.
+		// Check validity of lattice.
+		if (LOG) {
+			for (int t = 0; t < K; ++t) {
+				for (int z = 0; z < N; ++z) {
+					for (int y = 0; y < N; ++y) {
+						for (int x = 0; x < N; ++x) {
+							Occupied occupied = lattice.occupied(t, { x, y, z });
+							if (occupied != Occupied::LINE && occupied != Occupied::EMPTY) {
+								std::cerr
+									<< "Error 8: Invalid occupied # "
+									<< static_cast<int>(occupied) << " at "
+									<< "(" << x << ", " << y << ", " << z << ", " << t << ")" << std::endl;
+								return 8;
+							}
+							if (occupied == Occupied::LINE) {
+								Index3 next = lattice.next_site(t, Index3 { x, y, z });
+								Occupied next_occupied = lattice.occupied(t + 1, next);
+								if (next_occupied == Occupied::EMPTY) {
+									std::cerr
+										<< "Error 8: Connected from line to empty at "
+										<< "(" << x << ", " << y << ", " << z << ", " << t << ")" << std::endl;
+									return 8;
+								}
+								if (mod(lattice.prev_site(t + 1, next), N) != Index3 { x, y, z }) {
+									std::cerr
+										<< "Error 8: One way connection from "
+										<< "(" << x << ", " << y << ", " << z << ", " << t << ")" << std::endl;
+									return 8;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 		// Particle number.
 		unsigned n = 0;
 		for (int z = 0; z < N; ++z) {
 			for (int y = 0; y < N; ++y) {
 				for (int x = 0; x < N; ++x) {
-					if (lattice.occupied(0, Index3 { x, y, z })) {
+					Occupied occupied = lattice.occupied(0, Index3 { x, y, z });
+					if (occupied == Occupied::LINE) {
 						n += 1;
 					}
 				}
